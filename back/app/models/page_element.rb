2cf866b08161dbf8760258ad0f3c4bfc6dce92d5
Erika Komori <komoriakirarika@example.com>
2024-11-15 16:59:38 +0900

    PageにAPIが叩けるようにバックエンドを実装

--- back/app/models/page_element.rb
+++ back/app/models/page_element.rb
@@ -1,3 +1,14 @@
 class PageElement < ApplicationRecord
   belongs_to :page
+
+  # Enumの定義
+  enum element_type: {
+    object: 'object',
+    nature: 'nature',
+    text: 'text',
+    background: 'background'
+  }
+
+  validates :element_type, presence: true
+  validates :content, presence: true
 end
