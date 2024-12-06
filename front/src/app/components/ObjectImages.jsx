b434bca5f66114dce941e232e4f894c89828b72c
Erika Komori <komoriakirarika@example.com>
2024-12-06 17:23:54 +0900

    要素選択時にカーソルを指のアイコンに変更し

--- front/src/app/components/ObjectImages.jsx
+++ front/src/app/components/ObjectImages.jsx
@@ -16,6 +16,7 @@ export default function ObjectImages({ onImageSelect }) {
             alt={`Person ${index + 1}`}
             width={48}
             height={48}
+            className="cursor-pointer"
             onClick={() => {
               console.log("Image clicked:", src);
               onImageSelect && onImageSelect(src, "もの");
